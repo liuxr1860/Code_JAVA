f95b117b3de6a87ffa3f5ee523b0428250669ba1
liuxr <liuxr1861@163.com>
2018-05-21 23:19:12 +0800

    2018-05-21 imocc

---
+++ sendMessage.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+public class sendMessage {
+float screen;
+float cpu;
+float mem;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my first eclipse code");
+System.out.println("hahahha");
+	}
+
+}
